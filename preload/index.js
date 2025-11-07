e140f91af77dbc5dcddc3332f8e7a9fc24d23133
swlws <979623074@qq.com>
2025-11-07 09:08:24 +0800

    api

--- preload/index.js
+++ preload/index.js
@@ -1,3 +1,6 @@
-const { version, contextBridge } = require("electron");
+const { version, contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld("versions", process.versions);
+contextBridge.exposeInMainWorld("__context_bridge__", {
+  version: process.versions,
+  ping: ipcRenderer.invoke("ping"),
+});
--- public/index.js
+++ public/index.js
@@ -1,7 +1,7 @@
 const information = document.getElementById("info");
 
 let versionString = "";
-Object.keys(window.versions).forEach((key) => {
-  versionString += `${key}: ${window.versions[key]}\n`;
+Object.keys(window.__context_bridge__.version).forEach((key) => {
+  versionString += `${key}: ${window.__context_bridge__.version[key]}\n`;
 });
 information.innerText = versionString;

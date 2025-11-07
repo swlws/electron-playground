b55cee4b78aa177ab001bd4deb5cce51d365c703
swlws <979623074@qq.com>
2025-11-07 09:08:38 +0800

    api

--- preload/index.js
+++ preload/index.js
@@ -2,5 +2,5 @@ const { version, contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("__context_bridge__", {
   version: process.versions,
-  ping: ipcRenderer.invoke("ping"),
+  ping: () => ipcRenderer.invoke("ping"),
 });
